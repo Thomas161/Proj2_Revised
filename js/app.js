9fe027343653561dec1904ac6df3746e961351d7
Tom Magin <magintc@mac.com>
2020-09-26 17:28:13 +1000

    #lost

--- js/app.js
+++ js/app.js
@@ -24,14 +24,12 @@ window.addEventListener("DOMContentLoaded", function (evt) {
   console.log(navbarList.innerHTML); //hello
   const sections = document.querySelectorAll("section"); //Check length/existence of section tags
   for (var i = 0; i < sections.length; i++) {
-    console.log("Sections => ", sections[i].id); //id of each individual section tag
-    console.log(
-      "Sections getBoundingRect() => ",
-      sections[i].getBoundingClientRect()
-    );
+    // console.log("Sections => ", sections[i].id); //id of each individual section tag
+    // console.log(
+    //   "Sections getBoundingRect() => ",
+    //   sections[i].getBoundingClientRect()
+    // );
   }
-  //   const navItems = document.getElementsByClassName("menu__link");
-  const topButton = document.getElementsByClassName("button");
 
   /**
    * End Global Variables
@@ -53,8 +51,8 @@ window.addEventListener("DOMContentLoaded", function (evt) {
     // Automatically building links for all the sections on the page
     [...sections].forEach((sec) => {
       // for (var i = 0; i < sections.length; i++) {
-      console.log(sec.dataset);
-      //   console.log(sec.dataset.nav);
+      //   console.log(sec.dataset);
+      console.log(sec.dataset.nav);
       liElements += `<li><a class="menu_link ${sec.id}" href="#${sec.id}" >${sec.dataset.nav}</a></li>`;
     });
     navbarList.innerHTML = liElements;
@@ -67,84 +65,75 @@ window.addEventListener("DOMContentLoaded", function (evt) {
     for (let i = 0; i < sections.length; i++) {
       console.log(sections[i]);
       let positionSection = sections[i].getBoundingClientRect();
-      console.log(
-        "Top Position ",
-        positionSection.top,
-        "Bottom Position",
-        positionSection.bottom,
-        "Left Position",
-        positionSection.left,
-        "Right Position",
-        positionSection.right
-      );
-      if (
-        positionSection.top >= 0 &&
-        positionSection.left >= 0 &&
-        positionSection.right <=
-          (window.innerWidth || document.documentElement.clientWidth) &&
-        positionSection.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight)
-      ) {
-        let id = sections[i].getAttribute("id");
-        console.log(id);
+      //   console.log(
+      //     "Top Position ",
+      //     positionSection.top,
+      //     "Bottom Position",
+      //     positionSection.bottom,
 
-        document.querySelector(`${id}`).classList.add("active");
-        sections[i].classList.add("your-active-class");
+      if (positionSection.top <= 150 && positionSection.bottom >= 150) {
+        let identity = sections[i].getAttribute("id");
+        console.log("What is this ", identity); //8 id's in html doc
+
+        document.querySelector(`.${identity}`).classList.add("active");
+        sections[identity].classList.add("your-active-class");
         console.log("In Viewport");
       } else {
-        let id = sections[i].getAttribute("id");
-        console.log(id);
-        document.querySelector(`${id}`).classList.remove("active");
-        sections[i].classList.remove("your-active-class");
+        let identity = sections[i].getAttribute("id");
+        console.log(identity); //8 id's in html doc
+        document.querySelector(`.${identity}`).classList.remove("active");
+        sections[identity].classList.remove("your-active-class");
+
         console.log("Not in viewport");
       }
     }
   }
 
-  //   console.log(sections[i]);
   document.addEventListener("scroll", () => {
     addActiveClassWhenInViewport();
   });
 
-  let a = document.getElementsByTagName("a"); //undefined
-  console.log("a href tags", a[0].href);
-  //   for (let a in aHref)
-  for (let i = 0; i < a.length; i++) {
-    a[i].href.addEventListener("click", (event) => {
+  const abc = document.querySelector("a").href;
+  console.log(abc); //3
+  // Scroll to section on link click
+  for (let i = 0; i < abc.length; i++) {
+    console.log(abc[i]);
+    // let aRefs = document.querySelectorAll("a");
+    console.log(abc[i].href); //3
+    abc[i].href.addEventListener("click", (event) => {
       event.preventDefault();
 
-      //   const aHref = document.getElementsByTagName("a").href;
-      //   let scrollId = document.querySelector(aHref[i]);
-
-      a[i].href.scrollIntoView({
+      abc.scrollIntoView({
         behavior: "smooth",
       });
     });
   }
 
+  const topButton = document.getElementById("button");
+
   //call function below when window is scrolled
-  //   window.onscroll = function () {
-  //     scrollButtonIntoView();
-  //   };
+  window.onscroll = function () {
+    scrollButtonIntoView();
+  };
 
   //hide button until you have scrolled beyond 20px of top doc
-  //   function scrollButtonIntoView() {
-  //     if (
-  //       document.body.scrollTop > 20 ||
-  //       document.documentElement.scrollTop > 20
-  //     ) {
-  //       topButton.style.display = "block";
-  //     } else {
-  //       topButton.style.display = "none";
-  //     }
-  //   }
+  function scrollButtonIntoView() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      topButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+    }
+  }
 
   //click button, scrolls to top of html doc
-  //   topButton.addEventListener("click", function (e) {
-  //     e.preventDefault();
+  topButton.onclick = function (e) {
+    e.preventDefault();
 
-  //     window.scrollTo({ top: 0, behavior: "smooth" });
-  //   });
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 });
 /**
  * End Main Functions
@@ -153,7 +142,5 @@ window.addEventListener("DOMContentLoaded", function (evt) {
  */
 
 // Build menu
-
-// Scroll to section on link click
 
 // Set sections as active
