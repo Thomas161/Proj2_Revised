20cd055e1a78615788e0c027f9dd26244d809b04
Tom Magin <magintc@mac.com>
2020-09-28 15:17:04 +1000

    #ready for submission

--- js/app.js
+++ js/app.js
@@ -24,15 +24,14 @@ window.addEventListener("DOMContentLoaded", function (evt) {
   console.log(navbarList.innerHTML); //hello
   const sections = document.querySelectorAll("section"); //Check length/existence of section tags
   console.log(sections.length);
-  for (var i = 0; i < sections.length; i++) {
-    // console.log("Sections => ", sections[i].id); //id of each individual section tag
-    // console.log(
-    //   "Sections getBoundingRect() => ",
-    //   sections[i].getBoundingClientRect()
-    // );
-  }
-
   const topButton = document.getElementById("button");
+  // for (var i = 0; i < sections.length; i++) {
+  // console.log("Sections => ", sections[i].id); //id of each individual section tag
+  // console.log(
+  //   "Sections getBoundingRect() => ",
+  //   sections[i].getBoundingClientRect()
+  // );
+  // }
 
   /**
    * End Global Variables
@@ -51,13 +50,11 @@ window.addEventListener("DOMContentLoaded", function (evt) {
   const createNavLinks = () => {
     [...sections].forEach((sec) => {
       let liElements = document.createElement("li");
-      // for (var i = 0; i < sections.length; i++) {
-      let cl, ids, inner;
+      let cl, ids;
       cl = liElements.className.add = "menu__link";
       console.log(cl); //menu__link
       ids = liElements.id = "nav-" + sec.id;
       console.log(ids); //nav-section1-4
-      // inner = ;
       console.log((liElements.innerText = sec.dataset.nav)); //section 1-4
       navbarList.appendChild(liElements);
       liElements.addEventListener("click", function () {
@@ -80,16 +77,11 @@ window.addEventListener("DOMContentLoaded", function (evt) {
         let top = sections[i].getBoundingClientRect().top;
         let bottom = sections[i].getBoundingClientRect().bottom;
 
-        if (
-          // console.log(sectionById) //null
-          top <= 150 &&
-          bottom >= 150
-        ) {
+        if (top <= 150 && bottom >= 150) {
           console.log("In viewport");
-          // itemId.classList.add("your-active-class");
+
           sections[i].classList.add("your-active-class");
         } else {
-          // itemId.classList.remove("your-active-class");
           console.log("Not in viewport");
           sections[i].classList.remove("your-active-class");
         }
