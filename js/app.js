83e31e4d0a50a5b904398ffa118279d7e905b1a9
Tom Magin <magintc@mac.com>
2021-10-07 17:59:25 +1100

    added parallax scrolling, styled navbar, provided education section

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", function (evt) {
   const sections = document.querySelectorAll("section");
   const intro = document.querySelector("#intro");
   const skills = document.querySelector("#skills");
+  const parallax = document.querySelector(".wrapper");
   let t1 = gsap.timeline();
   console.log(t1);
 
@@ -19,7 +20,7 @@ window.addEventListener("DOMContentLoaded", function (evt) {
   playAnimation();
 
   let skillsAnimation = () => {
-    t1.fromTo(skills, 0.5, { y: -200, opacity: 0 }, { y: 0, opacity: 1 });
+    t1.fromTo(skills, 0.5, { y: -30, opacity: 0 }, { y: 0, opacity: 1 });
   };
   skillsAnimation();
 
@@ -38,26 +39,27 @@ window.addEventListener("DOMContentLoaded", function (evt) {
     });
   };
 
+  window.addEventListener("scroll", () => {
+    let offset = window.scrollY;
+    // console.log("offset", offset);
+    // console.log("offset * 0.7", offset * 0.7);
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  });
   // Add class 'active' to section when near top of viewport
 
-  function addActiveClassWhenInViewport() {
+  const addActiveClassWhenInViewport = () => {
     window.addEventListener("scroll", (event) => {
       for (let i = 0; i < sections.length; i++) {
-        console.log(sections[i].getBoundingClientRect());
         let top = sections[i].getBoundingClientRect().top;
         let bottom = sections[i].getBoundingClientRect().bottom;
-
         if (top <= 150 && bottom >= 150) {
-          console.log("In viewport");
-
           sections[i].classList.add("your-active-class");
         } else {
-          console.log("Not in viewport");
           sections[i].classList.remove("your-active-class");
         }
       }
     });
-  }
+  };
 
   document.addEventListener("scroll", () => {
     addActiveClassWhenInViewport();
